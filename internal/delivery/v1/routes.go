8d64cf889b246030338c5144d2ac3e530fb8ec34
aguradev <wahmayun.wm@gmail.com>
2023-11-07 23:04:45 +0800

    add utils file and jwt library

--- internal/delivery/v1/routes.go
+++ internal/delivery/v1/routes.go
@@ -1,7 +1,9 @@
 package v1
 
 import (
+	"net/http"
 	routes "woman-center-be/internal/delivery/http"
+	"woman-center-be/utils/middlewares"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -10,8 +12,12 @@ import (
 
 func InitRoutes(e *echo.Echo, db *gorm.DB, validator *validator.Validate) {
 
-	v1 := e.Group("/api/v1")
+	v1 := e.Group("/api/v1", middlewares.Logger())
 
 	routes.HttpUserRoute(v1, db, validator)
+
+	v1.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to woman center api")
+	})
 
 }
---
+++ utils/middlewares/logger.go
@@ -0,0 +1,12 @@
+package middlewares
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func Logger() echo.MiddlewareFunc {
+	return middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	})
+}
