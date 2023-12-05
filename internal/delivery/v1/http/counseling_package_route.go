f35e319629cba5af15295a2a4b60dfcd083e198a
sayyidhhashuro <sayyidhhashuro@gmail.com>
2023-12-06 00:03:57 +0700

    add endpoint mobile for user

--- internal/delivery/v1/http/counseling_package_route.go
+++ internal/delivery/v1/http/counseling_package_route.go
@@ -25,4 +25,8 @@ func HttpCounselingPackageRoute(group *echo.Group, db *gorm.DB, validate *valida
 	verifyTokenAdmin.POST("/counseling-packages", CounselingHandler.CreatePackage)
 	verifyTokenAdmin.GET("/counseling-packages/:title", CounselingHandler.FindByTitle)
 	verifyTokenAdmin.GET("/counseling-packages", CounselingHandler.GetAllPackage)
+
+	verifyToken := group.Group("", middlewares.VerifyTokenSignature("SECRET_KEY"))
+	cpackageUser := verifyToken.Group("/counseling-packages")
+	cpackageUser.GET("", CounselingHandler.GetAllPackage)
 }
