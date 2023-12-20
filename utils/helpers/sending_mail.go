989af565d006f2f73170c3b2b843aab2e03521f9
aguradev <wahmayun.wm@gmail.com>
2023-12-20 21:15:38 +0800

    setup email template

--- utils/helpers/sending_mail.go
+++ utils/helpers/sending_mail.go
@@ -1,7 +1,9 @@
 package helpers
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 
 	"github.com/spf13/viper"
 	"gopkg.in/gomail.v2"
@@ -11,6 +13,14 @@ type EmailRequest struct {
 	Subject string
 	To      string
 	Content string
+}
+
+type EmailDataRequest struct {
+	Name_customer  string
+	package_name   string
+	Counselor_name string
+	Date_schedule  string
+	Total          string
 }
 
 func SendingEmail(request EmailRequest) error {
@@ -30,5 +40,45 @@ func SendingEmail(request EmailRequest) error {
 	}
 
 	return nil
+
+}
+
+func SendingEmailWithHTML(to string, subject string, template string, request EmailDataRequest) error {
+	result, errResults := ParseTemplateEmail("utils/html_template/"+template, request)
+
+	if errResults != nil {
+		return errResults
+	}
+
+	Message := gomail.NewMessage()
+	Message.SetHeader("From", viper.GetString("email.USER"))
+	Message.SetHeader("To", to)
+	Message.SetHeader("Subject", subject)
+	Message.SetBody("text/html", result)
+
+	d := gomail.NewDialer(viper.GetString("EMAIL.HOST"), viper.GetInt("EMAIL.PORT"), viper.GetString("EMAIL.USER"), viper.GetString("EMAIL.PASS"))
+
+	if errSending := d.DialAndSend(Message); errSending != nil {
+		return errSending
+	}
+
+	return nil
+}
+
+func ParseTemplateEmail(templateFileName string, data interface{}) (string, error) {
+
+	t, errTemplate := template.ParseFiles(templateFileName)
+
+	if errTemplate != nil {
+		return "", errTemplate
+	}
+
+	buf := new(bytes.Buffer)
+
+	if errExce := t.Execute(buf, data); errExce != nil {
+		return "", errExce
+	}
+
+	return buf.String(), nil
 
 }
